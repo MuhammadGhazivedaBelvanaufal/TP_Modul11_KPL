60c471c04e614c107ba7ead18d4a637b254ecab1
belvanaufal <qhikizaalicia@gmail.com>
2024-05-05 23:23:22 +0700

    Tugas Pendahuluan 11 KPL

---
+++ AljabarApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace AljabarApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AljabarLibrary aljabarLibrary = new AljabarLibrary();
+
+            // Mencari akar-akar dari persamaan kuadrat
+            Console.Write("Masukkan koefisien persamaan kuadrat (a, b, c) dipisahkan dengan spasi: ");
+            string[] input = Console.ReadLine().Split();
+            double[] persamaanKuadrat = new double[3];
+            for (int i = 0; i < 3; i++)
+            {
+                persamaanKuadrat[i] = double.Parse(input[i]);
+            }
+
+            double[] akarPersamaan = aljabarLibrary.AkarPersamaanKuadrat(persamaanKuadrat);
+            Console.WriteLine("AkarPersamaanKuadrat({0}, {1}, {2})", persamaanKuadrat[0], persamaanKuadrat[1], persamaanKuadrat[2]);
+            if (double.IsNaN(akarPersamaan[0]) && double.IsNaN(akarPersamaan[1]))
+            {
+                Console.WriteLine("Persamaan kuadrat memiliki akar imajiner.");
+            }
+            else
+            {
+                Console.WriteLine("Output: {{{0}, {1}}}", akarPersamaan[0], akarPersamaan[1]);
+            }
+            Console.WriteLine();
+
+            // Mendapatkan hasil kuadrat dari persamaan berpangkat 1
+            Console.Write("Masukkan koefisien persamaan linear (a, b) dipisahkan dengan spasi: ");
+            input = Console.ReadLine().Split();
+            double[] persamaanLinear = new double[2];
+            for (int i = 0; i < 2; i++)
+            {
+                persamaanLinear[i] = double.Parse(input[i]);
+            }
+
+            double[] hasilKuadrat = aljabarLibrary.HasilKuadrat(persamaanLinear);
+            Console.WriteLine("HasilKuadrat({0}, {1})", persamaanLinear[0], persamaanLinear[1]);
+            Console.WriteLine("Output: {{{0}, {1}, {2}}}", hasilKuadrat[0], hasilKuadrat[1], hasilKuadrat[2]);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ AljabarLibraries/AljabarLibrary.cs
@@ -0,0 +1,41 @@
+﻿namespace AljabarLibraries
+{
+    public class AljabarLibrary
+    {
+        public double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+            double diskriminan = b * b - 4 * a * c;
+            double akar1, akar2;
+
+            if (diskriminan > 0)
+            {
+                akar1 = (-b + Math.Sqrt(diskriminan)) / (2 * a);
+                akar2 = (-b - Math.Sqrt(diskriminan)) / (2 * a);
+            }
+            else if (diskriminan == 0)
+            {
+                akar1 = akar2 = -b / (2 * a);
+            }
+            else
+            {
+                akar1 = akar2 = double.NaN; // Menandakan akar imajiner
+            }
+
+            return new[] { akar1, akar2 };
+        }
+
+        public double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double a2 = a * a;
+            double b2 = 2 * a * b;
+            double c2 = b * b;
+
+            return new[] { a2, b2, c2 };
+        }
+    }
+}
